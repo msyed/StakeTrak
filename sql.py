35104a94e35eef2fe90edc2090d9c831bfcad72f
Milton Syed <msyed@Miltons-MacBook-Air.local>
2016-01-08 19:43:37 -0500

    query function

---
+++ sql.py
@@ -0,0 +1,15 @@
+import sqlite3 as lite
+import sys
+
+
+con = lite.connect('ASG.db')
+
+with con:    
+    
+    cur = con.cursor()    
+    cur.execute("SELECT * FROM ENTITIES")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print row[1]
