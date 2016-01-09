ad1ed1e5d5ccc18e49ed396f734c38762bcac9f1
Sameer Mehra <sammysignal@gmail.com>
2016-01-09 16:47:08 -0500

    spacing

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,13 @@
 
-
 $(document).ready(function(){
 
-	$('#search-db').on('click', function(){
-		$('#search-form').toggle();
-		$('#options-container').toggle();
-	});
+  $('#search-db').on('click', function(){
+    $('#search-form').toggle();
+    $('#options-container').toggle();
+  });
 
-	$('#go-back').on('click', function(){
-		$('#options-container').toggle();
-		$('#search-form').toggle();
-	});
-
+  $('#go-back').on('click', function(){
+    $('#options-container').toggle();
+    $('#search-form').toggle();
+  });
 })
