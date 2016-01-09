a3811cb045d0e72a3b1e102b7395ef17e743f970
Sameer Mehra <sammysignal@gmail.com>
2016-01-09 16:39:21 -0500

    frontpage js toggling

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+
+	$('#search-db').on('click', function(){
+		$('#search-form').toggle();
+		$('#options-container').toggle();
+	});
+
+	$('#go-back').on('click', function(){
+		$('#options-container').toggle();
+		$('#search-form').toggle();
+	});
+
+})
