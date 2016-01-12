b0a3826e8daf36d94c1062c74c65e3301b13f347
Sameer Mehra <sammysignal@gmail.com>
2016-01-12 11:04:49 -0500

    second attempt at nlp function

--- nlp.py
+++ nlp.py
@@ -1,11 +1,22 @@
+# pip install numpy
 import nltk
+from extractText import extractText
+
 def extract_entities(text):
-     for sent in nltk.sent_tokenize(text):
-         for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
-             if hasattr(chunk, 'node'):
-                 print chunk.node, ' '.join(c[0] for c in chunk.leaves())
+	for sent in nltk.sent_tokenize(text):
+		for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+			if hasattr(chunk, 'node'):
+				print chunk.node, ' '.join(c[0] for c in chunk.leaves())
 
 
-x = "Barack Obama is the president of the United States"
 
-extract_entities(x)
+def extract_entities2(text):
+    pos = nltk.pos_tag(text.split())
+    keywords = [noun for noun in pos if (noun[1] == 'NNP' or noun[1] == 'NN' or noun[1] == 'NNS')]
+    return keywords
+
+
+x = extractText('police.txt')
+
+#print extract_entities(x)
+print extract_entities2(x)
